21fb382958ab8fe6a6dfcb6eeef25a78c9deee28
SW-GAGAN <srgaganreddy0@gmail.com>
2021-09-03 21:51:27 +0530

    Added: Ability to update an employee from the payroll details

--- js/EmployeePayroll.js
+++ js/EmployeePayroll.js
@@ -1,61 +1,61 @@
 //UC-9
-class EmployeePayrollData {
+class EmployeePayrollData{
 
-    get id() { return this._id; }
-    set id(id) {
-        this._id = id;
+    get id(){ return this._id; }
+    set id(id){
+        this._id=id;
     }
 
-    get name() { return this._name; }
-    set name(name) {
+    get name(){ return this._name; }
+    set name(name){
         let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
-        if (nameRegex.test(name)) {
-            this._name = name;
+        if( nameRegex.test(name)){
+            this._name=name;
         }
-        else throw 'Name is Incorrect';
+        else throw 'Name is Incorrect';  
+    }
+    
+    get profilepic(){ return this._profilepic; }
+    set profilepic(profilepic){
+        this._profilepic=profilepic;
     }
 
-    get profilepic() { return this._profilepic; }
-    set profilepic(profilepic) {
-        this._profilepic = profilepic;
+    get gender(){ return this._gender; }
+    set gender(gender){
+        this._gender=gender;
+    }
+    
+    get department(){ return this._department; }
+    set department(department){
+        this._department=department;
+    }
+    
+    get salary(){ return this._salary; }
+    set salary(salary){
+        this._salary=salary;
     }
 
-    get gender() { return this._gender; }
-    set gender(gender) {
-        this._gender = gender;
+    get note(){ return this._note; }
+    set note(note){
+        this._note=note;
     }
 
-    get department() { return this._department; }
-    set department(department) {
-        this._department = department;
-    }
-
-    get salary() { return this._salary; }
-    set salary(salary) {
-        this._salary = salary;
-    }
-
-    get note() { return this._note; }
-    set note(note) {
-        this._note = note;
-    }
-
-    get startDate() { return this._startDate; }
-    set startDate(startDate) {
+    get startDate(){ return this._startDate; }
+    set startDate(startDate){
         // let now = new Date();
         // if(startDate > now) throw 'Start Date is a future Date';
         // var diff = Math.abs(now.getTime() - startDate.getTime());
         // if(diff / (1000 * 60 * 60 * 24) > 30)
         //     throw 'Start Date is beyond 30 days';
-        this._startDate = startDate;
+        this._startDate=startDate;
     }
 
-    toString() {
+    toString(){
         const options = { year: 'numeric', month: 'long', day: 'numeric' };
         const empDate = !this.startDate ? "undefined" :
-            this.startDate.toLocaleDateString("en-us", options);
-        return "id= " + this.id + ", name='" + this.name + ", gender='" + this.gender +
-            ", profilepic='" + this.profilepic + ", department='" + this.department +
-            ", salary='" + this.salary + ", startDate='" + empDate + ", note='" + this.note;
+                        this.startDate.toLocaleDateString("en-us", options);
+        return "id= " +this.id +", name='"+this.name + ", gender='" + this.gender +
+                    ", profilepic='" + this.profilepic + ", department='" + this.department +
+                    ", salary='" + this.salary + ", startDate='" + empDate + ", note='" + this.note; 
     }
 }
--- js/home.js
+++ js/home.js
@@ -1,4 +1,5 @@
 // UC-17
+// UC-21
 let empPayrollList;
 window.addEventListener('DOMContentLoaded', (event) => {
     empPayrollList = getEmployeePayrollDataFromStorage();
@@ -9,10 +10,10 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
 const createInnerHTML = () => {
     const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>startDate</th><th>Actions</th>";
-    if (empPayrollList.length == 0) return;
+    if(empPayrollList.length == 0) return;
     let innerHtml = `${headerHtml}`;
     // let empPayrollList = createEmployeePayrollJSON();
-    for (const empPayrollData of empPayrollList) {
+    for(const empPayrollData of empPayrollList) {
         innerHtml = `${innerHtml}
         <tr>
             <td>
@@ -28,13 +29,13 @@ const createInnerHTML = () => {
                 <img id="${empPayrollData._id}" onclick="update(this)" src="../assets/icons/create-black-18dp.svg" alt="edit">
             </td>
         </tr>`;
-    }
+        }
     document.querySelector('#table-display').innerHTML = innerHtml;
 }
 
 const getDeptHtml = (deptList) => {
     let deptHtml = '';
-    for (const dept of deptList) {
+    for(const dept of deptList){
         deptHtml = `${deptHtml} <div class='dept-label'>${dept}</div>`
     }
     return deptHtml;
@@ -48,11 +49,11 @@ const getEmployeePayrollDataFromStorage = () => {
 // UC-20
 const remove = (node) => {
     let empPayrollData = empPayrollList.find(empData => empData._id == node.id);
-    if (!empPayrollData) return;
+    if(!empPayrollData) return;
     const index = empPayrollList
-        .map(empData => empData._id)
-        .indexOf(empPayrollData._id);
-    empPayrollList.splice(index, 1);
+                    .map(empData => empData._id)
+                    .indexOf(empPayrollData._id);
+    empPayrollList.splice(index,1);
     localStorage.setItem("EmployeePayrollList", JSON.stringify(empPayrollList));
     document.querySelector(".emp-count").textContent = empPayrollList.length;
     createInnerHTML();
